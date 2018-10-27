db3260c308b4815e05dddedf834c686e0322883e
yura91 <uvv911@mail.ru>
2018-10-27 20:06:28 +0400

    delete getCityListResponsesObservable()

--- app/src/main/java/com/dager/mvpdagger2retrofitroomrxjava/network/ApiControllerRetrofit.java
+++ app/src/main/java/com/dager/mvpdagger2retrofitroomrxjava/network/ApiControllerRetrofit.java
@@ -2,23 +2,15 @@ package com.dager.mvpdagger2retrofitroomrxjava.network;
 
 import com.dager.mvpdagger2retrofitroomrxjava.network.pojo.Rss;
 
-import io.reactivex.Observable;
 import io.reactivex.Single;
 import retrofit2.Response;
 import retrofit2.http.GET;
-
 
 
 public interface ApiControllerRetrofit {
 
     @GET("hubs/all")
     Single<Response<Rss>> getCityListResponses();
-
-    @GET("/CityListResponses")
-    Observable<Rss> getCityListResponsesObservable();
-
-
-
 
 
 }
